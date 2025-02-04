7ae648409c11e7bc7daac4cb5c77c643b4a8b70c
Sean O'Beirne <sean.t.obeirne@gmail.com>
2025-02-04 12:43:34 -0500

    basic input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to RuneScape counter: Please select a skill to track");
+
+    let mut skill_choice: String = String::new();
+
+    // std::io::stdin().read_line(&mut skill_choice);
+    // correct because we handle the Result with expect
+    io::stdin().read_line(&mut skill_choice).expect("Failed");
+    let skill_choice = skill_choice.trim();
+    // skill_choice = String::from(skill_choice.trim());
+
+    println!("{skill_choice}")
 }
